51e8ea6918e5da157da3ad9282384264689dd7b0
REZAUL KARIM <110754378+rkarimone@users.noreply.github.com>
2023-12-13 13:47:50 +0600

    Update 0001_Proxmox_With_TrueNAS.sh

--- 0001_Proxmox_With_TrueNAS.sh
+++ 0001_Proxmox_With_TrueNAS.sh
@@ -1,51 +1,31 @@
 
 
-  471  fdisk -l
-  472  ll /dev/disk/by-id/
-  473  ls -lah /dev/disk/by-id/
-  474  ls -lah /dev/disk/by-id/
-  475  fdisk -l
-  476  ls -lah /dev/disk/by-id/
-  477  cat /etc/pve/storage.cfg
-  478  cat /etc/iscsi/initiatorname.iscsi
-  479  apt search freenas-proxmox
-  480  keyring_location=/usr/share/keyrings/ksatechnologies-truenas-proxmox-keyring.gpg
-  481  curl -1sLf 'https://dl.cloudsmith.io/public/ksatechnologies/truenas-proxmox/gpg.284C106104A8CE6D.key' |  gpg --dearmor >> ${keyring_location}
-  482  vim /etc/apt/sources.list.d/ksatechnologies-repo.list
-  483  apt update
-  484  cat /etc/apt/sources.list
-  485  vim /etc/apt/sources.list.d/ksatechnologies-repo.list
-  486  apt update
-  487  vim /etc/apt/sources.list.d/ksatechnologies-repo.list
-  488  vim /etc/apt/sources.list.d/ksatechnologies-repo.list
-  489  vim /etc/apt/sources.list.d/ksatechnologies-repo.list
-  490  apt update
-  491  apt search freenas-proxmox
-  492  apt install freenas-proxmox
-  493  apt full upgrade
-  494  apt full-upgrade
-  495  uptime
-  496  date
-  497  systemctl restart freenas-proxmox
-  498  /etc/init.d/open-iscsi restart
-  499  mkdir /etc/pve/priv/zfs
-  500  ls -lah /etc/pve/priv/zfs
-  501  ls
-  502  ll
-  503  ls -lah
-  504  cp .ssh/id_rsa.pub /etc/pve/priv/zfs/
-  505  cp .ssh/id_rsa.pub /etc/pve/priv/zfs/172.23.88.200_id_rsa.pub
-  506  cd /etc/pve/priv/zfs/
-  507  ls
-  508  rm id_rsa.pub
-  509  ls
-  510  ll
-  511  ssh-copy-id -i /etc/pve/priv/zfs/172.23.88.200_id_rsa.pub root@172.23.88.200
-  512  cp /root/.ssh/id_rsa /etc/pve/priv/zfs/172.23.88.200_id_rsa
-  513  ssh-copy-id -i /etc/pve/priv/zfs/172.23.88.200_id_rsa.pub root@172.23.88.200
-  514  ssh 'root@172.23.88.200'
-  515  cd
-  516  history
+
+### PROXMOX ZFS OVER iSCSI ###
+  
+  cat /etc/pve/storage.cfg
+  cat /etc/iscsi/initiatorname.iscsi
+  apt search freenas-proxmox              // Will not be found! //
+
+  keyring_location=/usr/share/keyrings/ksatechnologies-truenas-proxmox-keyring.gpg
+  curl -1sLf 'https://dl.cloudsmith.io/public/ksatechnologies/truenas-proxmox/gpg.284C106104A8CE6D.key' |  gpg --dearmor >> ${keyring_location}
+  
+  vim /etc/apt/sources.list.d/ksatechnologies-repo.list
+  deb [signed-by=/usr/share/keyrings/ksatechnologies-truenas-proxmox-keyring.gpg] https://dl.cloudsmith.io/public/ksatechnologies/truenas-proxmox/deb/debian any-version main
+  apt update
+  apt search freenas-proxmox
+  apt install freenas-proxmox
+  apt full-upgrade
+
+  systemctl restart freenas-proxmox
+  /etc/init.d/open-iscsi restart
+  mkdir /etc/pve/priv/zfs
+  cp .ssh/id_rsa.pub /etc/pve/priv/zfs/172.23.88.200_id_rsa.pub     // 172.23.88.200 ip of TrueNAS Node //
+
+  cp /root/.ssh/id_rsa /etc/pve/priv/zfs/172.23.88.200_id_rsa
+  ssh-copy-id -i /etc/pve/priv/zfs/172.23.88.200_id_rsa.pub root@172.23.88.200
+  ssh 'root@172.23.88.200'
+
 
 
 
